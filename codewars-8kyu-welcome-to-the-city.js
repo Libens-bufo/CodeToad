00048ac2ddd3f896cec2637b75ec96f9c9e5c37c
Jason Swinehart <jaswine@comcast.net>
2021-11-23 22:22:57 -0500

    attempt

---
+++ codewars-8kyu-welcome-to-the-city.js
@@ -0,0 +1,6 @@
+function sayHello( name, city, state ) {
+    let names = "";
+    name.forEach(x => names += x + " ");
+    //names.trim()
+    return  `Hello, ${names.trim()}! Welcome to ${city}, ${state}!`
+  }
