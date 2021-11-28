47b24a283df9f22696737f831a6fa17dd07bda55
Jason Swinehart <jaswine@comcast.net>
2021-11-27 22:23:34 -0500

    working solution

--- codewars-8kyu-welcome-to-the-city.js
+++ codewars-8kyu-welcome-to-the-city.js
@@ -1,6 +1,6 @@
 function sayHello( name, city, state ) {
     let names = "";
     name.forEach(x => names += x + " ");
-    //names.trim()
+    names.trim()
     return  `Hello, ${names.trim()}! Welcome to ${city}, ${state}!`
   }
