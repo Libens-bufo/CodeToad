d377d5dfd2542a8fd610bbfc99dee8bd134729bb
Jason Swinehart <jaswine@comcast.net>
2022-01-04 22:24:37 -0500

    refactor

---
+++ codewars-8kyu-check-same-case.js
@@ -0,0 +1,12 @@
+function sameCase(a, b){
+    if(b.toLowerCase() === b.toUpperCase() || a.toUpperCase() === a.toLowerCase()){
+       return -1;
+     }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+         return 1;
+     }else{
+       return 0;
+     }
+   }
+//
+//
+//
