d1d0d075e4fc4ac81876ed1dfafd62ff5c79327c
Jason Swinehart <jaswine@comcast.net>
2022-02-20 20:23:39 -0500

    create file

--- CC-2022-05/codewars-7kyu-ghostbusters.js
+++ CC-2022-05/codewars-7kyu-ghostbusters.js
@@ -2,3 +2,8 @@ function ghostBusters(b) {
     if (b.includes(" ")) return b.split(' ').join('')
     return "You just wanted my autograph didn't you?"
   }
+
+ 
+    
+    
+    
--- CC-2022-05/codewars-8kyu-remove-first-and-last-character.js
+++ CC-2022-05/codewars-8kyu-remove-first-and-last-character.js
@@ -6,4 +6,4 @@ function removeChar(str){
    
    };
    
-///
+////
