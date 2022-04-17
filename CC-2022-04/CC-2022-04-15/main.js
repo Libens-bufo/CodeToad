e7892bae8d5b2aab6efd20edad915acf9ea3cc21
Jason Swinehart <jaswine@comcast.net>
2022-04-17 12:10:02 -0400

    comments added

--- CC-2022-04/CC-2022-04-15/main.js
+++ CC-2022-04/CC-2022-04-15/main.js
@@ -29,7 +29,7 @@ string.isUpper(, string
 // typeof "John"                 // Returns "string"
 // typeof 3.14                   // Returns "number"
 // typeof NaN                    // Returns "number"
-// typeof false                  // Returns "boolean"]
+// typeof false                  // Returns "boolean"
 // typeof [1,2,3,4]              // Returns "object"
 // typeof {name:'John', age:34}  // Returns "object"
 // typeof new Date()             // Returns "object"
