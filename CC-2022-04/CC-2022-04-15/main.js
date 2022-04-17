6874f6fc645706f114057095fe040fa981ff2f34
Jason Swinehart <jaswine@comcast.net>
2022-04-16 22:44:01 -0400

    with '#' will be ignored, and an empty message aborts the commit.

--- CC-2022-04/CC-2022-04-15/main.js
+++ CC-2022-04/CC-2022-04-15/main.js
@@ -18,3 +18,21 @@
 // 'B' and 'g' returns 0
 
 // '0' and '?' returns -1
+// things to check: isletter, case
+
+Functio
+const isLetter = char => char.toUpperCase()
+
+//Comment Appendix of useful info:
+string.isUpper(, string 
+//////////////////// typof operator /////////////////////////
+// typeof "John"                 // Returns "string"
+// typeof 3.14                   // Returns "number"
+// typeof NaN                    // Returns "number"
+// typeof false                  // Returns "boolean"]
+// typeof [1,2,3,4]              // Returns "object"
+// typeof {name:'John', age:34}  // Returns "object"
+// typeof new Date()             // Returns "object"
+// typeof function () {}         // Returns "function"
+// typeof myCar                  // Returns "undefined" *
+// typeof null                   // Returns "object" 
