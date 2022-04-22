5a298aa6c9c44103f06dbca27aaca077953c4e61
Jason Swinehart <jaswine@comcast.net>
2022-04-21 21:39:54 -0400

    exercises

--- CC-2022-04/CC.2022.04.21/main.js
+++ CC-2022-04/CC.2022.04.21/main.js
@@ -1,1 +1,20 @@
-//
+/* https://eloquentjavascript.net/02_program_structure.html
+Exercises
+
+1. 
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+for (let aLine = '#'; aLine.length < 8; aLine += '#'){
+    console.log(aLine)
+   
+}
