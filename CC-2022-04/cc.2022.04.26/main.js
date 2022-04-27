3697f822b6469ec54b01807300b7ea0b6b4748fe
Jason S <100892022+Libens-bufo@users.noreply.github.com>
2022-04-26 21:37:48 -0400

    //CodeWars - Reduce but Grow

---
+++ CC-2022-04/cc.2022.04.26/main.js
@@ -0,0 +1,8 @@
+//CodeWars - Reduce but Grow
+
+function grow(x){
+return x.reduce((a,b) => a*b)
+}
+
+//https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
