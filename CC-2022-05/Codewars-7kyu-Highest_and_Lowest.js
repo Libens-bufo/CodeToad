c0726c4186f62c65c685a11b3ca63b0534874b22
Jason Swinehart <jaswine@comcast.net>
2022-05-15 12:38:22 -0400

    sln to Highest and Lowest

---
+++ CC-2022-05/Codewars-7kyu-Highest_and_Lowest.js
@@ -0,0 +1,6 @@
+function highAndLow(n){
+    console.log(n.length,n[0], n[n.length-1]);
+    if (n.length == 1) return `${n[0]} ${n[0]}`;
+    let s =  n.split(' ').sort((a,b) => a - b);
+    return `${s[s.length - 1]} ${s[0]}`;
+  }
