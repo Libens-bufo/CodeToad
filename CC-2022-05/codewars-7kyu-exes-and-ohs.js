bd330c8d127b830a3f097ecf23f38fcaacd9a553
Jason Swinehart <jaswine@comcast.net>
2022-05-20 16:53:37 -0400

    create

--- CC-2022-05/codewars-7kyu-exes-and-ohs.js
+++ CC-2022-05/codewars-7kyu-exes-and-ohs.js
@@ -1,1 +1,9 @@
-//
+function XO(str) {
+    let xs = ''
+    let os = ''
+    str.toLowerCase().split('').forEach( x => {
+      if (x == 'x') xs += x;
+      if (x =='o') os += x;
+    })
+  return xs.length == os.length;
+}
