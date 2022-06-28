4733841750edf85b14dd6b6e953db095e5d9ecfc
Jason Swinehart <jaswine@comcast.net>
2022-06-28 08:50:39 -0400

    refactor

---
+++ codewars-7kyu-uncensor.js
@@ -0,0 +1,12 @@
+function uncensor(string, letters) {
+  
+    let arr = string.split('');
+    let letters2 = letters.split('')
+    for (let i = 0; i < letters2.length; i++){
+      arr[arr.indexOf('*')] = letters2[i];
+    }
+    return arr.join('');
+  }
+  
+
+////
